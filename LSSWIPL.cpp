2a8d2e12d4ccd7e348db3e0161d668ddf2a5f305
orpdoji <97266005+orpdoji@users.noreply.github.com>
2022-10-28 15:05:36 -0700

    Finished the program

--- LSSWIPL.cpp
+++ LSSWIPL.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <string>
-
-
+using namespace std;
 int lengthOfLongestSubstring(string s) {
-    
+
+    //new idea:
+    //while looping through string, store both length and substring
+    //each character will be ran through a loop to compare with the substring
+    string temp = "";
+    string max = "";
+    bool dis = true;
+    for(int i = 0; i < s.length()-1;i++){
+        for(int j = 0; j < temp.length()-1; j++){
+            if(temp.length()==0){
+                break;
+            }
+            if(s.at(i)==temp.at(j)){
+                dis = false;
+                break;
+            }
+        }
+
+        if(!dis){
+            dis = true;
+            if(temp.length() > max.length()){
+                max = temp;
+            }
+            temp = "";
+            
+        }
+
+        temp += s.at(i);
+    }
+    return max.size();
 }
 int main(){
-    
+    cout<<lengthOfLongestSubstring("abcabcbb");
 }
